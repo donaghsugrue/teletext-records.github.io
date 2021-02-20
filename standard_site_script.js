d3422cf95738c1a7944e14d4634b44ec39503d24
donaghsugrue <donaghsugrue@gmail.com>
2021-02-20 12:55:43 +0000

    Test version of new Teletext Website

---
+++ standard_site_script.js
@@ -0,0 +1,6 @@
+const el = document.querySelector("#rokaia-moving-cover");
+
+el.addEventListener("mousemove", (e) => {
+  el.style.backgroundPositionX = -e.offsetX + "px";
+  el.style.backgroundPositionY = -e.offsetY + "px";
+});
